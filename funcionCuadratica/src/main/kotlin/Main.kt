2c798192f827844058f4dceddf3ccc40b7fdb6be
santimulet12 <santiagomulet2008@gmail.com>
2025-05-06 09:01:19 -0300

    feat: Cálculo de raíces de la función

--- funcionCuadratica/src/main/kotlin/Main.kt
+++ funcionCuadratica/src/main/kotlin/Main.kt
@@ -1,22 +1,48 @@
 package org.example
 
-fun calcularValores(a:Int, b:Int, c:Int):Unit{
+fun calcularValores(a:Double, b:Double, c:Double):Unit{
     println("TABLA DE VALORES")
     println("x | y")
     for (x:Int in -5..5){
-        var y:Int = a*x*x+b*x+c
+        var y:Double = a*x*x+b*x+c
         println("$x | $y")
+    }
+}
+fun calcularRaices(a:Double, b:Double, c:Double):Unit{
+    val dicriminante:Double = b*b-4.0*a*c //calculamos el discriminante
+
+    when{
+        dicriminante > 0.0 ->{
+            println("La función tiene 2 raices reales")
+
+            val raiz1 = (-b + Math.sqrt(dicriminante)/(2.0 * a)) //calculamos la raiz 1
+            val raiz2 = (-b - Math.sqrt(dicriminante)/(2.0 * a)) //calculamos la raiz 2
+
+            //Mostrar las raices con 2 decimales
+            println("Raiz 1 = %.2f".format(raiz1))
+            println("Raiz 2 = %.2f".format(raiz2))
+
+        }
+        dicriminante < 0.0 ->{
+            println("La función no tiene raices")
+        }
+        else ->{
+            println("La función tiene una raiz")
+            val raiz: Double = -b/2*a
+
+            println("Raiz = %.2f".format(raiz))
+        }
     }
 }
 
 fun main() {
     //Definición de los coeficientes
-    val a: Int = 5 //Co. Cuadratico
-    val b: Int = 2 //Co. Lineal
-    val c: Int = 6 //Ordenada al Origen
+    val a: Double = 1.0 //Co. Cuadratico
+    val b: Double = 5.0 //Co. Lineal
+    val c: Double = 6.0 //Ordenada al Origen
 
     // Validar que "a" no sea 0
-    if (a == 0){
+    if (a == 0.0){
         println("El coeficiente cuadrático no puede ser 0")
     }
 
@@ -26,5 +52,6 @@ fun main() {
     //Mostrar tabla de valores.
     calcularValores(a,b,c)
 
-
+    //Cálculo de raices
+    calcularRaices(a,b,c)
 }
